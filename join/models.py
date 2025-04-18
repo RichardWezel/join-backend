0d1d1099c735559acbda40e171dd517b65008e04
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-04-18 21:35:12 +0200

    python interpreter ausgewählt

--- join/models.py
+++ join/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=7)  # Farbcodes wie #ff4646
+    mail = models.EmailField()
+    password = models.CharField(max_length=100)
+    locked_in = models.BooleanField(default=False)
