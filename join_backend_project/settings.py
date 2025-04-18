e4dcd34225e4309f65a135ba3e69ba41871d7af2
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-04-18 21:16:34 +0200

    cors headers hinzugefügt

--- join_backend_project/settings.py
+++ join_backend_project/settings.py
@@ -31,15 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'join'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",  # z.B. wenn du VSCode Live Server nutzt
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000"
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
